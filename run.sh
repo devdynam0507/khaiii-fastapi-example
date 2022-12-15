d6b3b7c8546e21bb9a27bc2fff9667305070036b
devdynam0507 <wsnam0507@naver.com>
2022-12-15 19:51:16 +0900

    스크립트 수정

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 docker run -v $(pwd):/workspace/code \
-           -t dynam0507/khaiii-fastapi \
            -p 80:8080 \
+           -t dynam0507/khaiii-fastapi \
            main:app --host 0.0.0.0 --port 8080 --workers 2
